d25652dbc03116ccdb982d4a0a0d568fdbbd589f
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-15 11:20:37 +1200

    Added VillaDTO.

--- ModernVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ ModernVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,19 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 using ModernVilla_VillaAPI.Models;
+using ModernVilla_VillaAPI.Models.Dto;
 
 namespace ModernVilla_VillaAPI.Controllers
 {
-    [Route("api/ViilaAPI")]
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController:ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa>{
-                new Villa{Id=1, Name = "Pool View" },
-                new Villa{Id=2, Name = "Beach View" }
+            return new List<VillaDTO>{
+                new VillaDTO{Id=1, Name = "Pool View" },
+                new VillaDTO{Id=2, Name = "Beach View" }
             };
         }
     }
