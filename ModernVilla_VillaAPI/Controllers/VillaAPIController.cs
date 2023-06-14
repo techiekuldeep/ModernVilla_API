d98d676a9cecbf700d36bbc585f3c6497fe6c99a
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-15 11:31:50 +1200

    Added an endpint to get single villa.

--- ModernVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ ModernVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -16,5 +16,11 @@ namespace ModernVilla_VillaAPI.Controllers
         {
             return VillaStore.villaList;
         }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
+        }
     }
 }
