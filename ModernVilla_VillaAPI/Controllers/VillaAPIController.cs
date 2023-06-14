1da2bf158481496cfe047c55ea4b7a5ce5dba901
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-15 11:13:37 +1200

    Added an endpoint to get all the villas.

--- ModernVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ ModernVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using ModernVilla_VillaAPI.Models;
+
 namespace ModernVilla_VillaAPI.Controllers
 {
+    [Route("api/ViilaAPI")]
     [ApiController]
     public class VillaAPIController:ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>{
+                new Villa{Id=1, Name = "Pool View" },
+                new Villa{Id=2, Name = "Beach View" }
+            };
+        }
     }
 }
