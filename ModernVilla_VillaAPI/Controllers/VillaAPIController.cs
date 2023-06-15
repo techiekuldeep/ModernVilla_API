26ccae5cb177347d6a865f11d2f101f7200bfef5
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-15 12:02:08 +1200

    Added another overload for ProducesResponseType.

--- ModernVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ ModernVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -12,12 +12,20 @@ namespace ModernVilla_VillaAPI.Controllers
     public class VillaAPIController:ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<VillaDTO> GetVillas()
         {
             return Ok(VillaStore.villaList);
         }
 
         [HttpGet("{id:int}")]
+        //[ProducesResponseType(200, Type =typeof(VillaDTO))]
+        //[ProducesResponseType(200)]
+        //[ProducesResponseType(404)]
+        //[ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDTO> GetVilla(int id)
         { 
             if(id == 0)
