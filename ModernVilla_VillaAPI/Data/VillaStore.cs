50049554f0df4cb535d0d8e3cf1e3e314ac228c5
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-15 17:29:42 +1200

    Added HttpPut action method to update villa.

--- ModernVilla_VillaAPI/Data/VillaStore.cs
+++ ModernVilla_VillaAPI/Data/VillaStore.cs
@@ -5,8 +5,8 @@ namespace ModernVilla_VillaAPI.Data
     public static class VillaStore
     {
         public static List<VillaDTO> villaList = new List<VillaDTO>{
-                new VillaDTO{Id=1, Name = "Pool View" },
-                new VillaDTO{Id=2, Name = "Beach View" }
+                new VillaDTO{Id=1, Name = "Pool View", Sqft=150, Occupancy=4 },
+                new VillaDTO{Id=2, Name = "Beach View",Sqft=250, Occupancy=5 }
             };
     }
 }
