54251489767209bcf3cee624d8baa0f4d3b15a58
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-16 10:05:11 +1200

    Added DbContext and models for Entity Framework Core.

---
+++ ModernVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using ModernVilla_VillaAPI.Models;
+
+namespace ModernVilla_VillaAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
