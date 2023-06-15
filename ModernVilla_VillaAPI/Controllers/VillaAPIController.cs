ec4444f90eb9afab5217655511a9c72fceef58ca
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-15 17:03:14 +1200

    Added custom validation to have unique villa name.

--- ModernVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ ModernVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -38,11 +38,22 @@ namespace ModernVilla_VillaAPI.Controllers
         }
 
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<VillaDTO> CreateVilla(VillaDTO villaDTO)
         {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            //Custom validation to check if villa name is unique
+            if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa already Exists!");
+                return BadRequest(ModelState);
+            }
             if (villaDTO == null)
             {
                 return BadRequest(villaDTO);
--- ModernVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ ModernVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace ModernVilla_VillaAPI.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModernVilla_VillaAPI.Models.Dto
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
