6b24459c0671c888d7559eaa63bf042838e2782f
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-16 15:05:03 +1200

    Repository CleanUp.

---
+++ ModernVilla_VillaAPI/Repository/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModernVilla_VillaAPI.Data;
+using ModernVilla_VillaAPI.Models;
+using System.Linq.Expressions;
+using ModernVilla_VillaAPI.Repository.IRepository;
+
+namespace ModernVilla_VillaAPI.Repository
+{
+    public class Repository<T> : IRepository<T> where T:class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public async Task CreateAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await SaveAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            dbSet.Remove(entity);
+            await SaveAsync();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
