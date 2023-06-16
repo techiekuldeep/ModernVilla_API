f95d67d0ccd9266bc9756f8d7721b5690151d904
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-17 06:21:39 +1200

    Added Mapping Config to web project.

---
+++ ModernVilla_Web/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+using ModernVilla_Web.Models;
+using ModernVilla_Web.Models.Dto;
+
+namespace ModernVilla_Web
+{
+    public class MappingConfig:Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaUpdateDTO>().ReverseMap();
+        }
+    }
+}
