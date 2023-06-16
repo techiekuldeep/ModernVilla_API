e056bf3ec54f7a8ffd89f7bc2cca497887f10cd8
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-16 13:54:57 +1200

    Added Villa Repository Interface

---
+++ ModernVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using ModernVilla_VillaAPI.Models;
+using System.Linq.Expressions;
+
+namespace ModernVilla_VillaAPI.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked = true);
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        Task Save();
+    }
+}
