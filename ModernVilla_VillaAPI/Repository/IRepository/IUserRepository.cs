e687c03035a1665f612245cb66955298754b10b8
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-19 10:08:39 +1200

    Added User Repository.

---
+++ ModernVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using ModernVilla_VillaAPI.Models.Dto;
+using ModernVilla_VillaAPI.Models;
+
+namespace ModernVilla_VillaAPI.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
---
+++ ModernVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using ModernVilla_VillaAPI.Data;
+using ModernVilla_VillaAPI.Models;
+using ModernVilla_VillaAPI.Models.Dto;
+using ModernVilla_VillaAPI.Repository.IRepository;
+
+namespace ModernVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
