26197bd8093d810a75815f9d6530f5f93849d741
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-18 23:34:23 +1200

    Added Update and Delete Action Method for Villa Number.

---
+++ ModernVilla_Web/Models/VM/VillaNumberDeleteVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+using ModernVilla_Web.Models.Dto;
+
+namespace ModernVilla_Web.Models.VM
+{
+    public class VillaNumberDeleteVM
+    {
+        public VillaNumberDeleteVM()
+        {
+            VillaNumber = new VillaNumberDTO();
+        }
+        public VillaNumberDTO VillaNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> VillaList { get; set; }
+    }
+}
---
+++ ModernVilla_Web/Models/VM/VillaNumberUpdateVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+using ModernVilla_Web.Models.Dto;
+
+namespace ModernVilla_Web.Models.VM
+{
+    public class VillaNumberUpdateVM
+    {
+        public VillaNumberUpdateVM()
+        {
+            VillaNumber = new VillaNumberUpdateDTO();
+        }
+        public VillaNumberUpdateDTO VillaNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> VillaList { get; set; }
+    }
+}
