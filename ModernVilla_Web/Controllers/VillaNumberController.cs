030e1eacc4dde753ae4ce306501a7a56a0a5391a
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-18 23:18:43 +1200

    Display API Error Messages

--- ModernVilla_Web/Controllers/VillaNumberController.cs
+++ ModernVilla_Web/Controllers/VillaNumberController.cs
@@ -62,6 +62,13 @@ namespace ModernVilla_Web.Controllers
                 {
                     return RedirectToAction(nameof(IndexVillaNumber));
                 }
+                else
+                {
+                    if (response.ErrorMessages.Count > 0)
+                    {
+                        ModelState.AddModelError("ErrorMessages", response.ErrorMessages.FirstOrDefault());
+                    }
+                }
             }
             var resp = await _villaService.GetAllAsync<APIResponse>();
             if (resp != null && resp.IsSuccess)
