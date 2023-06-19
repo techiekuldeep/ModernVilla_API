d8438dc911ceb5dfec36f3bb18e570fe4673fb76
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-19 13:27:53 +1200

    Added Auth Service.

--- ModernVilla_Web/Services/AuthService.cs
+++ ModernVilla_Web/Services/AuthService.cs
@@ -1,6 +1,39 @@
-﻿namespace ModernVilla_Web.Services
+﻿using ModernVilla_Utility;
+
+using ModernVilla_Web.Models;
+using ModernVilla_Web.Models.Dto;
+using ModernVilla_Web.Services.IServices;
+
+namespace ModernVilla_Web.Services
 {
-    public class AuthService
+    public class AuthService : BaseService, IAuthService
     {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+        public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+        }
+
+        public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "/api/UsersAuth/login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegisterationRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "/api/UsersAuth/register"
+            });
+        }
     }
 }
--- ModernVilla_Web/Services/IServices/IAuthService.cs
+++ ModernVilla_Web/Services/IServices/IAuthService.cs
@@ -1,6 +1,10 @@
-﻿namespace ModernVilla_Web.Services.IServices
+﻿using ModernVilla_Web.Models.Dto;
+
+namespace ModernVilla_Web.Services.IServices
 {
     public interface IAuthService
     {
+        Task<T> LoginAsync<T>(LoginRequestDTO objToCreate);
+        Task<T> RegisterAsync<T>(RegisterationRequestDTO objToCreate);
     }
 }
