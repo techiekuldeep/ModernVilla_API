78d3f5466e8f934f9009fae89cc847e9e845feb0
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-23 11:22:57 +1200

    Added changes for Login and Registration with Identity.

--- ModernVilla_VillaAPI/Repository/UserRepository.cs
+++ ModernVilla_VillaAPI/Repository/UserRepository.cs
@@ -20,13 +20,15 @@ namespace ModernVilla_VillaAPI.Repository
         private string secretKey;
         private readonly IMapper _mapper;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         public UserRepository(ApplicationDbContext db, IConfiguration configuration,
-              UserManager<ApplicationUser> userManager, IMapper mapper)
+             UserManager<ApplicationUser> userManager, IMapper mapper, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
             _mapper = mapper;
             _userManager = userManager;
             secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _roleManager = roleManager;
         }
         public bool IsUniqueUser(string username)
         {
@@ -94,6 +96,11 @@ namespace ModernVilla_VillaAPI.Repository
                 var result = await _userManager.CreateAsync(user, registerationRequestDTO.Password);
                 if (result.Succeeded)
                 {
+                    if (!_roleManager.RoleExistsAsync("admin").GetAwaiter().GetResult())
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole("admin"));
+                        await _roleManager.CreateAsync(new IdentityRole("customer"));
+                    }
                     await _userManager.AddToRoleAsync(user, "admin");
                     var userToReturn = _db.ApplicationUsers
                         .FirstOrDefault(u => u.UserName == registerationRequestDTO.UserName);
