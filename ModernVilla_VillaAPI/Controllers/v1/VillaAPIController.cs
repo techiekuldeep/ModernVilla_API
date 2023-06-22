55bcd29b6587fe91f2707afc87954d2763644bf8
Kuldeep Singh <36822070+techiekuldeep@users.noreply.github.com>
2023-06-22 13:02:23 +1200

    Added Filters for occupancy in API for v1 VillaAPIController.

--- ModernVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ ModernVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -38,11 +38,20 @@ namespace ModernVilla_VillaAPI.Controllers.v1
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<APIResponse>> GetVillas()
+        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")] int? occupancy)
         {
             try
             {
-                IEnumerable<Villa> villaList = await _dbVilla.GetAllAsync();
+                IEnumerable<Villa> villaList;
+
+                if (occupancy > 0)
+                {
+                    villaList = await _dbVilla.GetAllAsync(u => u.Occupancy == occupancy);
+                }
+                else
+                {
+                    villaList = await _dbVilla.GetAllAsync();
+                }
                 _response.Result = _mapper.Map<List<VillaDTO>>(villaList);
                 _response.StatusCode = HttpStatusCode.OK;
                 return Ok(_response);
